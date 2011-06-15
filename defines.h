2e66f4b92ac73ba728f882e37ef44911455e8b0b
Ben <ben.daccache@gmail.com>
2011-06-15 12:34:50 +0200

    renaming

---
+++ defines.h
@@ -0,0 +1,37 @@
+/**
+ *
+ *       @file defines.h
+ *
+ *      @brief   defines file
+ *
+ *    @version  1.0
+ *       @date   12.06.2011 23:39:21
+ *
+ *     @author  Ben D. (BD), dbapps2@gmail.com
+ *
+ */
+
+
+/* Id3Rename defines */
+#define 	MAX_PATH	1024
+#define 	MAX_NAME	1024
+#define		MAX_ALL		2048
+#define 	MAX_ARTIST 	1024
+#define 	MAX_ALBUM 	1024
+#define 	MAX_TITLE	1024
+#define 	MAX_YEAR	4
+#define 	MAX_EXT		5
+#define 	MAX_TOK		4
+#define 	SUCCESS		0
+#define 	FAILURE		-1
+#define		EMPTY		1
+
+/* Pattern defines */
+#define 	SEPARATOR	"#"
+#define 	ARTIST		"ART"
+#define 	TITLE		"TIT"
+#define		YEAR		"YEAR"
+#define 	ALBUM		"ALB"
+#define 	GENRE		"GEN"
+#define		LENGTH		"LEN"
+
